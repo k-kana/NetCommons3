c80ebc3e627f10c1f9a2d33912c3db09f365beb5
s-nakajima <nakajimashouhei@gmail.com>
2018-08-31 16:21:51 +0900

    Merge composer/master into speedup2

--- tools/build/plugins/cakephp/travis/environment.sh
+++ tools/build/plugins/cakephp/travis/environment.sh
@@ -13,3 +13,6 @@ done
 export IGNORE_PLUGINS=`echo $IGNORE_PLUGINS | cut -c 2-`
 
 export PATH=$PATH:./vendors/bin
+
+phpenv config-add $NETCOMMONS_BUILD_DIR/tools/build/plugins/cakephp/travis/travis.php.ini
+php -ini | grep memory_limit
